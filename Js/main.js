e798a17e4a5f7d19b9f29b957295d9a5b5bba088
KalpanaWalhe <68656831+KalpanaWalhe@users.noreply.github.com>
2020-07-23 22:19:25 +0530

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,56 @@
+const responsive = {
+	0:{
+       items:1
+	},
+  320:{
+	  items:1
+  },
+  560: {
+	  items:2
+  },
+  960 :{
+    items:3
+  }
+}
+
+$(document).ready(function(){
+	
+	
+	$nav=$('.nav');
+	$toggleCollapse=$(".toggle-collapse");
+	
+	/**click event on toggle menu*/
+	$toggleCollapse.click(function(){
+		
+		$nav.toggleClass('collapse');
+	})
+
+
+	//owl-coursal for blog
+	$('.owl-carousel').owlCarousel({
+	  loop:true,
+	  autoplay:true,
+	  autoplayTimeout:3000,
+	  autoplayHoverPause:true,
+	  dots:false,
+	  nav:true,
+	  navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+	  responsive: responsive 
+	});
+
+    /*click to scroll up*/
+    $('.move-up span').click(function(){
+		$('html,body').animate({
+			scrollTop:0
+		},1000);
+		
+	})
+
+
+	//animation on scroll
+	AOS.init();
+
+
+  
+
+});
